15f4b97126f9a69a40af57c3ebe2b672dca60aee
Esteban <eginez@gmail.com>
2014-12-30 21:57:28 -0700

    working function for finding words in trie

--- src/triad/core.clj
+++ src/triad/core.clj
@@ -4,9 +4,12 @@
             [clojure.string :as sr])
   (:gen-class))
 
-(def mat [[ "a" "b" ]["d" "e" ]])
+(def mat [[ "a" "b" "m" ]["d" "a" "p" ]["c" "a" "r"]])
 
 (def english-words (sr/split-lines (slurp "/usr/share/dict/words")))
+
+(defn in? [elem col]
+  (some #(= elem %) col))
 
 (defn getat[mat [x y]]
   (nth (nth mat y) x))
@@ -35,7 +38,7 @@
 (defn startWalk [mat]
   (doseq [x (range (count mat))
           y (range (count (nth mat x)))]
-    (println (all-words-from-position mat [x y] (getat mat [x y]) [] []))))
+    (println (all-words-from-position mat [x y] (getat mat [x y]) []))))
 
 
 (defn insert-word-in-triad [triad letters pos word]
@@ -53,16 +56,21 @@
 
 (defn find-words-trie [trie mat new-pos word used-positions]
   (cond 
+    ;(nil? (get-in trie word)) (println (str "not a word: " word))
+    (nil? (get-in trie word)) (identity word)
     (empty? (get-in trie word)) (println word)
     :else (let [all-neig-pos (st/difference (set (neighbours mat new-pos)) (set (conj used-positions new-pos)))]
             (doseq [pos all-neig-pos]
-              (find-words-trie trie mat pos (str word (getat pos)) (conj used-positions new-pos))))))
+              (find-words-trie trie mat pos (str word (getat mat pos)) (conj used-positions new-pos))))))
+
+(defn find-all-word-in-trie [trie mat]
+  (doseq [x (range (count mat))
+          y (range (count (nth mat x)))]
+    (find-words-trie trie mat [x y] (getat mat [x y]) [])))
 
 (def english-words-trie (build-triad-dict {} english-words))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args])
-  
-
-
+  [& args]
+  (find-all-word-in-trie english-words-trie mat))
